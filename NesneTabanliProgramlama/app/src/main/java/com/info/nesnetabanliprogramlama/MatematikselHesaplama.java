54e3ce09393a7824330fc03658539d9bfdbe15c9
Mete Sayan <metesayan99@gmail.com>
2018-10-28 19:07:11 +0300

    Nesne Tabanlı Programlama

---
+++ NesneTabanliProgramlama/app/src/main/java/com/info/nesnetabanliprogramlama/MatematikselHesaplama.java
@@ -0,0 +1,25 @@
+package com.info.nesnetabanliprogramlama;
+
+public class MatematikselHesaplama {
+
+    public void topla(int sayi1,int sayi2){
+        System.out.println("Toplam : "+(sayi1+sayi2));
+    }
+
+    public void topla(double sayi1,int sayi2){
+        System.out.println("Toplam : "+(sayi1+sayi2));
+    }
+
+    public void topla(int sayi1,double sayi2){
+        System.out.println("Toplam : "+(sayi1+sayi2));
+    }
+
+    public void topla(int sayi1,int sayi2, String isim){
+        System.out.println("Toplam : "+(sayi1+sayi2) + " : " + isim);
+    }
+
+    public void topla(double sayi1,double sayi2){
+        System.out.println("Toplam : "+(sayi1+sayi2));
+    }
+
+}
---
+++ NesneTabanliProgramlama/app/src/main/java/com/info/nesnetabanliprogramlama/OverloadingMain.java
@@ -0,0 +1,15 @@
+package com.info.nesnetabanliprogramlama;
+
+public class OverloadingMain {
+
+    public static void main(String args[]){
+
+        MatematikselHesaplama mh = new MatematikselHesaplama();
+
+        mh.topla(4.59,30 );
+
+        mh.topla(40, 30, "Mete");
+
+    }
+
+}
