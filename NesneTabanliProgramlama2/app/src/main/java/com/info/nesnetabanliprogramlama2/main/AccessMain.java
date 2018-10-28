c2db4528c74ec3bb9c034978d11a88cdcafef0ee
Mete Sayan <metesayan99@gmail.com>
2018-10-29 01:49:44 +0300

    Nesne Tabanlı Programlama(OOP) 2

---
+++ NesneTabanliProgramlama2/app/src/main/java/com/info/nesnetabanliprogramlama2/main/AccessMain.java
@@ -0,0 +1,18 @@
+package com.info.nesnetabanliprogramlama2.main;
+
+import com.info.nesnetabanliprogramlama2.object.Ogrenciler;
+
+public class AccessMain {
+
+    public static void main(String[] args) {
+
+        Ogrenciler o1 = new Ogrenciler();
+
+        o1.isim = "Ahmet";
+        o1.okulNo = 435 ;
+
+        o1.bilgiAl();
+
+    }
+
+}
---
+++ NesneTabanliProgramlama2/app/src/main/java/com/info/nesnetabanliprogramlama2/object/Ogrenciler.java
@@ -0,0 +1,13 @@
+package com.info.nesnetabanliprogramlama2.object;
+
+public class Ogrenciler {
+
+    public String isim;
+    public int okulNo;
+
+    public void bilgiAl(){
+        System.out.println("Öğrenci adı: " + isim);
+        System.out.println("Öğrencinin okul no: "+okulNo);
+    }
+
+}
