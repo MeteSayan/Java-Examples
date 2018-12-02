b75fe7a324a1bbaf6cddfe5e37550849ac884b5c
Mete Sayan <metesayan99@gmail.com>
2018-12-02 23:02:18 +0300

    Android Tasarım

---
+++ AndroidTasarim/app/src/main/java/com/info/androidtasarim/MainActivity.java
@@ -0,0 +1,13 @@
+package com.info.androidtasarim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_ekran_tasarim);
+    }
+}
